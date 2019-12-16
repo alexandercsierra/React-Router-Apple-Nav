54a4f702b10e833a97119bbf125c312857b97785
Alexander Sierra <alexandercsierra@gmail.com>
2019-12-16 17:11:28 -0500

    created components

---
+++ applenav/src/components/NavWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SubNav from './SubNav'
+
+const NavWrapper = () =>{
+
+    return (
+        <div>
+            NavWrapper
+            <SubNav />
+        </div>
+    )
+}
+
+export default NavWrapper
